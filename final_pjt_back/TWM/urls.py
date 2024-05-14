a03da5c8bb347928d26320526640ad2218cf5821
there2989 <there2989@gmail.com>
2024-05-14 17:34:28 +0900

    feat, chore: community 게시글과 댓글 CRUD 구현, Comment 모델에 user 삭제. 추후 ArticleSeriallzizer fields 확인필요

--- final_pjt_back/TWM/urls.py
+++ final_pjt_back/TWM/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/signup/', include('dj_rest_auth.registration.urls')),
     # path('movies/', include('movies.urls')),
-    # path('community/', include('community.urls')),
+    path('community/', include('community.urls')),
 ]
